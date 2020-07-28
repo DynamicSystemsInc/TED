3f199da0cd9dd26477d5bd897b1fda16fe36427f
Glenn Faden <glenn.faden@dynamicsystemsinc.com>
2020-07-28 20:22:50 +0000

    remove obsolete mate source files

--- ws/userland/src/components/desktop/mate/mate-calc/mate-calc-1.22.1/src/utility.h
+++
@@ -1,8 +0,0 @@
-#ifndef UTILITY_H
-#define UTILITY_H
-
-#include <glib/gi18n.h>
-
-extern GSettings *g_settings_var;
-
-#endif
--- ws/userland/src/components/desktop/mate/mate-panel/mate-panel-1.22.1/applets/wncklet/wncklet-resources.h
+++
@@ -1,7 +0,0 @@
-#ifndef __RESOURCE_wncklet_H__
-#define __RESOURCE_wncklet_H__
-
-#include <gio/gio.h>
-
-extern GResource *wncklet_get_resource (void);
-#endif
